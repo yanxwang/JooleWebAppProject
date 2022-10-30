3ea857981b5d59cd489f9a0f0c247b858d3b037e
yanxwang <61066724+yanxwang@users.noreply.github.com>
2022-10-30 15:18:08 -0500

    Delete tblUser.sql

--- tblUser.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[tblUser]
-(
-	[User_ID] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
-	[User_Name] varchar (50) NULL,
-	[User_Email] varchar (50),
-	[User_Image] IMAGE,
-	[User_Password] varchar (50)
-)
